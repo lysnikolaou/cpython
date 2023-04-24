bb0775f2542876ede8164b6f1c522e3094e82590
Guido van Rossum <guido@python.org>
2023-04-24 11:56:56 -0600

    Create new token TAGSTRING_START

--- Parser/token.c
+++ Parser/token.c
@@ -66,6 +66,7 @@ const char * const _PyParser_TokenNames[] = {
     "TYPE_COMMENT",
     "SOFT_KEYWORD",
     "FSTRING_START",
+    "TAGSTRING_START",
     "FSTRING_MIDDLE",
     "FSTRING_END",
     "COMMENT",
